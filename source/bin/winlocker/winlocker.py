b2507ccdeaf60fc718baae66db66be4f1b9d62d7
Stanislav <stas-chubar@yandex.ru>
2022-12-29 17:57:13 +0700

    minor update. start project winlocker.py

---
+++ source/bin/winlocker/winlocker.py
@@ -0,0 +1,12 @@
+# Импортируем все модули, которые нам пригодятся
+from playsound import *
+import tkinter
+from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+import getpass
+import sys
+import os
+import os.path
+import pyautogui
+from time import sleep
